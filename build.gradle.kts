e88ab5d135d1cfcd8fe88ca05d52b13fc8e6137f
root <root@t>
2020-08-02 10:15:05 +0800

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,28 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val gradle_version = "6.6-rc-3"
+val gradle_version = "6.6-rc-4"
+
+buildscript {
+    extra.apply {
+        //set("kotlinVersion", "1.3.72")
+        set("kotlinVersion", "1.4.0-rc")
+    }
+
+    repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+	maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+	maven { url = uri("https://repo.eclipse.org/content/repositories/paho-snapshots") }
+	//flatDir { dirs = "lib" }
+    }
+}
 
 plugins {
 	id("org.springframework.boot") version "2.4.0-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.4.0-rc"
-	kotlin("plugin.spring") version "1.4.0-rc"
+	kotlin("jvm") version "${property("kotlinVersion")}"
+	kotlin("plugin.spring") version "${property("kotlinVersion")}"
 }
 
 group = "com.y"
